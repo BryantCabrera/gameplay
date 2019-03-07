b8f478af24828bf5fbcaaf39ebbb70751df6700c
Bryant <cabrera.bryant@gmail.com>
2019-03-07 15:58:38 -0800

    Adds userListQuery to Users.js.

--- src/Users/Users.js
+++ src/Users/Users.js
@@ -3,5 +3,40 @@ import { graphql, compose } from 'react-apollo';
 import gql from "graphql-tag";
 
 const Users = ({data, mutate}) => {
+    if(data.loading){
+        return <p>Loading...</p>
+    }
+
+    if(data.error){
+        return <p>{data.error.message}</p>
+    }
+
+    const userList = data.getUsers.map((user) => {
+        return <li key={user.id}>
+                  {user.userame}
+                  {user.email}
+                  {user.img}
+                  {user.games}
+              </li>
+        })
     
+      return (
+        <ul>
+          {userList}
+        </ul>
+        )
 }
+
+export const userListQuery = gql`
+  query {
+    getUsers {
+      id
+      username
+      email
+      img
+      games
+    }
+  }
+`
+
+export default graphql(userListQuery)(Users);
