2f15de68fa5a1b05c99c5eb5be1add49c0fd62f6
Bryant <cabrera.bryant@gmail.com>
2019-03-08 14:59:09 -0800

    Creates navbar with link, styling, and animation in NavBar.js.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,13 +3,54 @@ import { withRouter, NavLink } from 'react-router-dom';
 import logo from '../../logo.svg';
 import './NavBar.css';
 
+let open = false;
 
 class NavBar extends Component {
+    state = {
+        open: false,
+        buttonText: '+',
+        overlayClass: '',
+        wrapperClass: ''
+    }
+
+    openNav = () => {
+        this.setState({
+            open: true,
+            buttonText: '-',
+            overlayClass: ' on-overlay',
+            wrapperClass: ' opened-nav'
+        });
+    }
+
+    closeNav = () => {
+        this.setState({
+            open: false,
+            buttonText: '+',
+            overlayClass: '',
+            wrapperClass: ''
+        });
+    }
+
+    handleClick = (e) => {
+        // if (!e) {
+        //     let e = window.event;
+        // }
+        //prevents triggering click event on document
+        e.stopPropagation();
+
+        if (!this.state.open) {
+            this.openNav();
+        }
+        else {
+            this.closeNav();
+        }
+    }
+
     render () {
         return (
             <div className="nav">
-                <button id="nav__button" className="nav__button">+</button>
-                <nav className="nav__navbar">
+                <button id="nav__button" className="nav__button" onClick={this.handleClick}>{this.state.buttonText}</button>
+                <nav className={`nav__navbar${this.state.wrapperClass}`}>
                     {/* <img src={logo} alt="Game Play Logo" title="Game Play Logo"></img> */}
                     <ul className="nav__navbar__links">
                         <li><NavLink exact to="/">Home</NavLink></li>
@@ -22,6 +63,7 @@ class NavBar extends Component {
 
                         <li><NavLink exact to="/users">Users</NavLink></li>
                     </ul>
+                    <div id="nav__overlay" className={`nav__overlay${this.state.overlayClass}`}></div>
                 </nav>
             </div>
         )
