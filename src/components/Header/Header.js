b0311a97f6e607d0d528b4da791f036e1e116836
Bryant <cabrera.bryant@gmail.com>
2019-03-10 17:01:12 -0700

    Fixes logo url in Header.js.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { withRouter, NavLink } from 'react-router-dom';
-import logo from '../../static/imgs/logo.svg';
+// import logo from '../../static/imgs/logo.svg';
 import './Header.css'
 
 class Header extends Component {
     render() {
         return (
             <header className="header">
-                <NavLink exact to="/" className="header__logo--link"><img className="header__logo" src={logo} alt="Game Play Logo" title="Game Play Logo"></img></NavLink>
+                <NavLink exact to="/" className="header__logo--link">
+                    <img className="header__logo" src="https://i.imgur.com/Y40XSUQ.png" alt="Game Play Logo" title="Game Play Logo"></img></NavLink>
                 <div className="header__bryant">
                     created by: Bryant Cabrera
                 </div>
